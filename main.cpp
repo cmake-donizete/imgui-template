18d8113f1d64bcb6db2aa9445f0b01e0ddd549d3
DonizeteVida <donizetevida1998@outlook.com>
2025-07-07 22:34:22 +0200

    Add SDL3 into cmake target

--- main.cpp
+++ main.cpp
@@ -1,5 +1,54 @@
-#include <iostream>
+#define SDL_MAIN_USE_CALLBACKS 1
 
-int main(int, char**){
-    std::cout << "Hello, from img-gui-template!\n";
+#include <SDL3/SDL.h>
+#include <SDL3/SDL_main.h>
+
+static SDL_Window *window;
+static SDL_Renderer *renderer;
+
+SDL_AppResult SDL_AppInit(void **appstate, int argc, char *argv[])
+{
+    if (!SDL_Init(SDL_INIT_VIDEO))
+    {
+        SDL_Log("Couldn't initialize SDL: %s", SDL_GetError());
+        return SDL_APP_FAILURE;
+    }
+
+    if (!SDL_SetHint(SDL_HINT_MAIN_CALLBACK_RATE, "60"))
+    {
+        SDL_Log("Couldn't set framerate: %s", SDL_GetError());
+        return SDL_APP_FAILURE;
+    }
+
+    if (!SDL_CreateWindowAndRenderer(
+            "window",
+            300,
+            300,
+            SDL_WINDOW_RESIZABLE,
+            &window,
+            &renderer))
+    {
+        SDL_Log("Couldn't create window/renderer: %s", SDL_GetError());
+        return SDL_APP_FAILURE;
+    }
+
+    return SDL_APP_CONTINUE;
+}
+
+SDL_AppResult SDL_AppEvent(void *appstate, SDL_Event *event)
+{
+    if (event->type == SDL_EventType::SDL_EVENT_QUIT)
+    {
+        return SDL_AppResult::SDL_APP_SUCCESS;
+    }
+    return SDL_AppResult::SDL_APP_CONTINUE;
+}
+
+SDL_AppResult SDL_AppIterate(void *appstate)
+{
+    return SDL_AppResult::SDL_APP_CONTINUE;
+}
+
+void SDL_AppQuit(void *appstate, SDL_AppResult result)
+{
 }
